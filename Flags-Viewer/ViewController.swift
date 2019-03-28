97e269a9d87fb1c1b2f345fa3c2fe8925cca8833
Ignasi Perez <ignasiperez@gmail.com>
2019-03-28 10:11:17 +0100

    Initial Commit

---
+++ Flags-Viewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flags-Viewer
+//
+//  Created by ignasiSwift on 28/03/2019.
+//  Copyright © 2019 ignasiSwift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
